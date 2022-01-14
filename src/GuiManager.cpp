5b1a0e7fe9004780b5cb6657a28d16e3dab96b48
J0llyver <j0llyver.grain@gmail.com>
2022-01-14 09:56:40 +0100

    Created get and delete functions for windows

--- src/GuiManager.cpp
+++ src/GuiManager.cpp
@@ -24,10 +24,24 @@ void GuiManager::renderGui() {
 
 std::shared_ptr<gui::Window> GuiManager::createWindow(const std::string &name, const uint32_t width,
                                                       const uint32_t height) {
-  windows.insert(std::make_pair(name, std::make_shared<gui::Window>(name, width, height)));
-  return windows[name];
+  if (windows.find(name) != windows.end()) {
+    windows.insert(std::make_pair(name, std::make_shared<gui::Window>(name, width, height)));
+    return windows[name];
+  } else {
+    return windows[name];
+  }
 }
 
-// std::shared_ptr<gui::Window> GuiManager::getWindow(const std::string &name) {
-//   return windows
-// }
+std::shared_ptr<gui::Window> GuiManager::getWindow(const std::string &name) {
+  if (windows.find(name) != windows.end()) {
+    return windows[name];
+  }
+
+  return nullptr;
+}
+
+void GuiManager::deleteWindow(const std::string &name) {
+  if (windows.find(name) != windows.end()) {
+    windows.erase(name);
+  }
+}
--- src/GuiManager.h
+++ src/GuiManager.h
@@ -15,6 +15,8 @@ class GuiManager {
 
   void renderGui();
   std::shared_ptr<gui::Window> createWindow(const std::string &name, const uint32_t width, const uint32_t height);
+  std::shared_ptr<gui::Window> getWindow(const std::string &name);
+  void deleteWindow(const std::string &name);
 
  private:
   GuiManager() = default;
